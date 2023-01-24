a67ee70f93951b26eec44eb834d6aff4fbd834dc
Lihuel <muracciole.al@gmail.com>
2023-01-24 15:17:23 +0700

    file base

---
+++ src/TodoCounter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter () {
+    return (
+        <section className='TodoCounter'>
+            <h1>Your Tasks</h1>
+            <h2>Completed 1 to 3 To Dos</h2>
+        </section>
+    );
+}
+
+export { TodoCounter };
